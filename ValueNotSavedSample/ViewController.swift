ed236f3591c1cbbde431f3ff5b677a6aae0e122b
ryo oomichi <oomichi@na-s.jp>
2020-01-17 15:09:52 +0900

    呼び出しコードを追加

--- ValueNotSavedSample/ViewController.swift
+++ ValueNotSavedSample/ViewController.swift
@@ -7,14 +7,14 @@
 //
 
 import UIKit
+import Keys
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        print(NankaProjectNameKeys().konoProjectDeTukauValue)
     }
-
-
 }
-
