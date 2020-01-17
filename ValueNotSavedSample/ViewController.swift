63530e404b627aeadf3a55e3edc6c0e91385dc04
ryo oomichi <oomichi@na-s.jp>
2020-01-17 14:57:36 +0900

    create project

---
+++ ValueNotSavedSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ValueNotSavedSample
+//
+//  Created by Ryo Oomichi on 2020/01/17.
+//  Copyright © 2020 Ryo Oomichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
